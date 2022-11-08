ef0d9fede4f9530731482e1509bea9fab5aaacb2
Baraka Danny <christophermutuku@CHRISTOPHERs-MacBook-Pro-2.local>
2022-11-08 22:22:23 +0300

    Delete unnecessary spaces

--- student.rb
+++ student.rb
@@ -2,11 +2,11 @@ require_relative 'Person'
 
 class Student < Person
 
-    def inititalize(age, classroom, name: 'unkown', parent_permission: true)
-        @classroom = classroom
-    end
+  def inititalize(age, classroom, name: 'unkown', parent_permission: true)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
