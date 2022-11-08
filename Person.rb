47165bb8fb3f9f5e72d1d0518022acf6e74781c2
Baraka Danny <christophermutuku@CHRISTOPHERs-MacBook-Pro-2.local>
2022-11-08 16:41:12 +0300

    update person class

--- Person.rb
+++ Person.rb
@@ -1,8 +1,8 @@
 class Person
-    attr_accessor : name, : age
-    attr_reader : id
+    attr_accessor :name, :age
+    attr_reader :id
 
-    def initialize(name: 'unkown', age, parent_permission: true)
+    def initialize(name, age, parent_permission = true)
         @id = Random.rand(1..1000)
         @name = name
         @age = age
