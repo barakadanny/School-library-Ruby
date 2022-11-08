e53cecff60d38a98edc6b8777e017c7b0ce87ae0
Baraka Danny <christophermutuku@CHRISTOPHERs-MacBook-Pro-2.local>
2022-11-08 22:26:27 +0300

    solve linters 1

--- person.rb
+++ person.rb
@@ -9,13 +9,15 @@ class Person
     @parent_permission = parent_permission
   end
 
-  public 
+  public
+
   def can_use_services?
     is_of_age? || @parent_permission
   end
 
   private
-  def is_of_age?
+
+  def of_age?
     @age >= 18
   end
 end
