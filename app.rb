654872541d8442ec326ac35edeb43427dc02d857
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-18 20:47:27 +0300

    list rentals method

--- app.rb
+++ app.rb
@@ -103,3 +103,21 @@ end
 
     puts 'Rental created successfully'
   end
+
+  def list_all_rentals
+    puts 'To see person rentals enter the person ID: '
+    @persons.each do |person|
+      puts "id: #{person.id}"
+    end
+    id = gets.chomp.to_i
+    puts 'Rented Books:'
+    @rentals.each do |rental|
+      if rental.person.id == id
+        puts "Peson: #{rental.person.name}  Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
+      else
+        puts
+        puts 'No records where found for the given ID'
+      end
+    end
+  end
+end
