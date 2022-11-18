9f3ea740ec0f7e26cfda2dfcf4a04cffb8954470
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-18 17:05:31 +0300

    create_student method

--- app.rb
+++ app.rb
@@ -17,10 +17,43 @@ class App
     @books.each { |book| puts "[Book] Title : #{book.title}, Author : #{book.author}" }
   end
 
-  def list_people
+  def list_persons
     puts 'there is no person!' if @persons.empty?
     @persons.each do |person|
       puts "[#{person.class.name}] Name: #{person.name}, Age: #{person.age}, id: #{person.id}"
     end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a Teacher (2)? [Input the number]: '
+    person_type = gets.chom
+    
+    case option
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid option'
+    end
+  end
+
+  def create_student
+    puts 'Create new Student'
+    Print 'Enter Age: '
+    age = gets.chomp.to_i
+    print 'Enter Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase
+    case parent_permission
+    when 'n'
+      student = Student.new(age, name, parent_permission: false)
+      @persons << student
+      puts 'Student does not have permission'
+    when 'y'
+      student = Student.new(age, name, parent_permission: true)
+      @persons << student
+      puts 'Student has permission'
   end
 end
