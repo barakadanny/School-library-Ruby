43db2d2138b1ba8f5fbbd3fc0066a4811a51acf9
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-18 16:49:30 +0300

    List_books class

--- app.rb
+++ app.rb
@@ -11,4 +11,9 @@ class App
     @books = []
     @rentals = []
   end
+
+  def list_books
+    puts 'there is no Books!' if @books.empty?
+    @books.each { |book| puts "[Book] Title : #{book.title}, Author : #{book.author}" }
+  end
 end
