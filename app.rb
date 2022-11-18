fb5292b2bd5a50c17c0d38887438458f8502d4cf
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-18 16:52:55 +0300

    List people method

--- app.rb
+++ app.rb
@@ -16,4 +16,11 @@ class App
     puts 'there is no Books!' if @books.empty?
     @books.each { |book| puts "[Book] Title : #{book.title}, Author : #{book.author}" }
   end
+
+  def list_people
+    puts 'there is no person!' if @persons.empty?
+    @persons.each do |person|
+      puts "[#{person.class.name}] Name: #{person.name}, Age: #{person.age}, id: #{person.id}"
+    end
+  end
 end
