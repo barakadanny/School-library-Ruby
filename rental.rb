2125ae38485821976d184a073fb8f93abf787ecf
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-19 14:04:07 +0300

    Update rental

--- rental.rb
+++ rental.rb
@@ -1,11 +1,11 @@
 class Rental
-  attr_accessor :book, :person, :date
+  attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
     @book = book
     @person = person
-    book.rentals.push(self)
-    person.rentals.push(self)
+    person.rental << self
+    book.rental << self
   end
 end
