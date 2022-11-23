9e788cd8992b165023edf653a27a41fd86de42be
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-23 19:04:02 +0300

    Add person test

---
+++ Rspec/spec/person_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../person.rb'
+require 'rspec/autorun' 
+describe Person do
+    context 'When testing the Person class' do
+      it 'should create a new person' do
+        person = Person.new(19, 'William')
+        id = person.id
+        expect(person.age).to eq 19
+        expect(person.name).to eq 'William'
+        expect(person.id).to eq id
+      end
+    end
+  end
