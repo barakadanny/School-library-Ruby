b2dd25aa5169db678d7375f955feb40d08da3d3f
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-23 16:03:56 +0300

    Book test added

---
+++ Rspec/spec/book_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../book'
+
+describe Book do
+  context 'When testing Book class' do
+    it 'Should create a book' do
+        book = Book.new('Java', 'danny')
+        expect(book.title).to eq('Java')
+        expect(book.author).to eq('danny')
+    end
+end
