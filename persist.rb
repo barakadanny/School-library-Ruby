995a8848652c957b9d01605bf9a2a0a2710649f7
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-23 13:21:15 +0300

    Update persist

--- persist.rb
+++ persist.rb
@@ -1,21 +1,21 @@
 require 'json'
 
 class Persist
-  attr_reader :path
+  attr_reader :file_path
 
-  def initialize(path)
-    @path = path
+  def initialize(file_path)
+    @file_path = file_path
   end
 
   def save(data)
-    myArr = JSON.pretty_generate(data)
-    File.write(file_path, myArr.to_s)
+    arr = JSON.pretty_generate(data)
+    File.write(file_path, arr.to_s)
   end
 
   def load
-    return [] unless File.exist?(@path)
+    return [] unless File.exist?(@file_path)
 
-    file = File.read(@path)
+    file = File.read(@file_path)
     JSON.parse(file)
   end
 end
