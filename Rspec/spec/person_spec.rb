3e247e56ffae70322c3ab183629c15f6f72501f6
Danny <danny@Dannys-MacBook-Pro.local>
2022-11-23 19:50:18 +0300

    Solve Linters

--- Rspec/spec/person_spec.rb
+++ Rspec/spec/person_spec.rb
@@ -1,14 +1,13 @@
 require_relative '../../person'
-require 'rspec/autorun' 
+require 'rspec/autorun'
 describe Person do
-    context 'When testing the Person class' do
-      it 'should create a new person' do
-        person = Person.new(19, 'William')
-        id = person.id
-        expect(person.age).to eq 19
-        expect(person.name).to eq 'William'
-        expect(person.id).to eq id
-      end
+  context 'When testing the Person class' do
+    it 'should create a new person' do
+      person = Person.new(19, 'William')
+      id = person.id
+      expect(person.age).to eq 19
+      expect(person.name).to eq 'William'
+      expect(person.id).to eq id
     end
   end
-  
+end
--- Rspec/spec/rental_spec.rb
+++ Rspec/spec/rental_spec.rb
@@ -4,14 +4,13 @@ require_relative '../../book'
 require 'rspec/autorun' 
 
 describe Book do
-    context 'When testing the Rental class' do
-      it 'should create a new Rental' do
-        teacher = Teacher.new('Biology', 64, 'Muwanika')
-        book = Book.new('Biological Science', 'D. J. Taylor')
-        rental = Rental.new('2022-11-09', teacher, book)
-        expect(rental.book.rental.length).to eq 1
-        expect(rental.person.rentals.length).to eq 1
-      end
+  context 'When testing the Rental class' do
+    it 'should create a new Rental' do
+      teacher = Teacher.new('Biology', 64, 'Muwanika')
+      book = Book.new('Biological Science', 'D. J. Taylor')
+      rental = Rental.new('2022-11-09', teacher, book)
+      expect(rental.book.rental.length).to eq 1
+      expect(rental.person.rentals.length).to eq 1
     end
   end
-  
+end
